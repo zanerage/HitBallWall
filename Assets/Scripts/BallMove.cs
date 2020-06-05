4b07c4c3023381276cd09446200d05f429c995a7
Mario Antolovic <marioantolovic0@gmail.com>
2020-06-05 18:17:44 +0200

    small android game movement and bouncing the ball

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+
+    Rigidbody2D myrigidbody;
+    public float bounceForce;
+
+
+    private void Awake() {
+        myrigidbody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          if(Input.anyKeyDown) {
+            StartBounce();
+        }
+    }
+
+    public void StartBounce() {
+        Vector2 randomDirection = new Vector2(Random.Range(-1,1),1);
+        myrigidbody.AddForce(randomDirection * bounceForce,ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/GameplayController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+
+    Rigidbody2D myrigidbody;
+    public float speed = 5;
+
+
+    private void Awake() {
+        myrigidbody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        TouchMove();
+    }
+
+    public void TouchMove() {
+        if(Input.GetMouseButton(0)) {
+            Vector2 currentPos = this.transform.position;
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if(touchPosition.x < currentPos.x) {
+
+                // we are moving left
+
+                myrigidbody.velocity = Vector2.left * speed;
+
+            }
+            else {
+                // or right
+
+                myrigidbody.velocity = Vector2.right * speed;
+
+            }
+        }
+
+        else {
+            myrigidbody.velocity = Vector2.zero;
+        }
+    }
+}
